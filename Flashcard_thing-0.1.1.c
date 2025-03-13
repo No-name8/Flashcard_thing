1475702d738a1e059650b2ebf8d4c3615b418efa
No-name8 <binetti5@outlook.com>
2025-03-13 11:57:46 +0000

    Add files via upload
    Changed method of getting questions and answers.

--- Flashcard_thing-0.1.1.c
+++ Flashcard_thing-0.1.1.c
@@ -2,39 +2,54 @@
 #include <string.h>
 #include <stdlib.h>
 
-char question[];
-char answr[]; 
-char usransw[];
+int q = 0;
+int a = 0;
+int ua = 0;
+char question[q];
+char answr[a]; 
+char usransw[ua];
 int i = 0; //Preserves the variable i so it can be used throughout the code. 
 int score = 0;
 int numquestions;
-char filename[];
+char filename[64];
 
 // takes questions from file
 char getinpt(FILE *file)
 {
 
-    char fout[64];
-
+    char fout;
     do {
-       fgets(fout, 64, file);
+        do {
+        fgets(fout, 1, file);
 
+            if ( q == 0)
+            {
+                q++;
+            }
+            else if (a == 0)
+            {
+                a++;
+            }
+            else if ( ua == 0)
+            {
+                ua++; 
+            }
+
+                  
+        } while( fout != '\n' || fout != EOF);
+        
         if(i % 2)
         {
-        strcpy(answr, fout);
+            answr = fout;
+            a++;
         } 
         else
         {
-        strcpy(question, fout);
+            question = fout; 
+            q++; 
         }
-        if (i  % 2)
-        {
-            i++;
-            break;
-        }
-    i++;      
-    } while( i <= numquestions );
-
+        i++;
+    } while(i <= numquestions);
    return 1;
 }
 
@@ -53,7 +68,7 @@ int main()
     scanf("%s", &filename);
     FILE *file = fopen( filename, "r");
    
-   if ((file == NULL)) 
+   if (file == NULL) 
    {
     perror("FILE does not exist"); 
     exit (27); 

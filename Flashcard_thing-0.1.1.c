882237d3797a7b61afedaf2a773268b1bdbb24c4
No-name8 <binetti5@outlook.com>
2025-03-13 23:49:02 +0000

    Doesn't work

--- Flashcard_thing-0.1.1.c
+++ Flashcard_thing-0.1.1.c
@@ -2,60 +2,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-int q = 0;
-int a = 0;
-int ua = 0;
-char question[q];
-char answr[a]; 
-char usransw[ua];
+
+
+char *question;
+char *answr; 
+char *usransw;
 int i = 0; //Preserves the variable i so it can be used throughout the code. 
 int score = 0;
 int numquestions;
-char filename[64];
+char filename[20];
 
 // takes questions from file
 char getinpt(FILE *file)
 {
-
-    char fout;
-    do {
-        do {
-        fgets(fout, 1, file);
-
-            if ( q == 0)
+    size_t buffersize = 20;
+    char *fout = malloc(buffersize * sizeof(char));
+do {
+        if( fgets(fout, buffersize, file) != NULL) 
+        {
+            while (fout[strlen(fout) - 1] != '\n' && !feof(file))
             {
-                q = strlen(fout);
-                question = malloc(sizof(fout));
+                buffersize *= 2;
+                fout = realloc(fout, buffersize * sizeof(char));
+                fgets(fout + strlen(fout), buffersize / 2, file);
             }
-            else if (a == 0)
-            {
-                a = strlen(fout);
-                answr = malloc(sizof(fout));
-            }
-
-
-                  
-        } while( fout != '\n' || fout != EOF);
         
-        if(i % 2)
-        {
-            a = strlen(fout);
-            answr =realloc(answr, sizof(a));
-            answr = fout;
-            
-        } 
-        else
-        {
-            q = strlen(fout);
-            question = realloc(question, sizeof(q));
-            question = fout; 
-            
-        }
-        i++;
-    } while(i <= numquestions);
-   return 1;
+        
+                if ( i % 2 != 0)
+                {
+                    question = malloc(strlen(fout) + 1);
+                    strcpy(question, fout);
+                }
+                else 
+                {
+                    answr = malloc(strlen(fout) + 1);
+                    strcpy(answr, fout);
+                }
+            }   
+            } while( *fout != '\n' && *fout != EOF);
+        i++; 
+        free(fout);   
+        return 1;
+    
 }
-
 // give questions to user
 
 // if true mark as learned
@@ -64,75 +53,79 @@ char getinpt(FILE *file)
 
 int main()
 {
+    char buffer[10];
+
     printf("welcome to Flashcard Thing\n");
     printf("How many questions would you like to answer?\n");
-    scanf("%d", &numquestions);
+    fgets(buffer, sizeof(buffer), stdin);
+    numquestions = strtol(buffer, NULL, 10);
+
+    while (getchar() != '\n');
+
     printf("Enter the name of the file for questions\n");
-    scanf("%s", &filename);
-    FILE *file = fopen( filename, "r");
+    scanf("%s", filename);
+    filename[strcspn(filename, "\n")] = 0;
+    
+    FILE *file = fopen( "name.txt", "r");
    
    if (file == NULL) 
-   {
-    perror("FILE does not exist"); 
-    exit (27); 
-   } 
-    while (i < numquestions)
-    {
-    getinpt(file);
+        {
+        perror("FILE does not exist"); 
+        exit (27); 
+        } 
+        while (i < numquestions)
+        {
+            getinpt(file);
 
-    printf("question: %s", question);
+            printf("question: %s", question);
 
-    strcmp(question, answr);
+        
 
-    switch(strcmp(question, answr))
-    {
-        case 0:
-            ua = strlen(answr);
-            usransw = malloc(sizeof(ua));
-            break;
-        case >0:
-            ua = strlen(question);
-            usransw = malloc(sizeof(ua));
-            break;
-        case <0:
-            ua = strlen(answr);
-            usransw = malloc(sizeof(ua));
-            break; 
-    }
-
-    scanf("%s", &usransw);
+            if ( strcmp( answr, question) < 0)
+                {
+                usransw = malloc(sizeof(answr));
+                }    
+                else if ( strcmp( answr, question) > 0)
+                { 
+                usransw = malloc(sizeof(question));
+                }    
+                else
+                {
+                usransw = malloc(sizeof(answr));
+                }
+        }
+    scanf("%s", usransw);
     
 
     if (strcmp(answr, usransw) == 0)
-    {
-        printf("Correct\n");
-        score++; 
-    }
-    else
-    {
-        printf("Incorrect\n");
-        // mark as incorrect somehow       
-        printf("answer: %s", answr);
-        score--;
-    }
-    if (score == numquestions )
-    {
-        printf("Congratulations on finishing you scored: %d out of %d", score, numquestions);
-        printf("Would you like to play again? (y/n)\n");
-        char playagain[2];
-        scanf("%c", &playagain);
-        if ((playagain == "y"))
         {
-            score = 0;
-            free (question);
-            free (answr);
-            free (usransw);
-            i = 0;
+            printf("Correct\n");
+            score++; 
         }
-        else
+    else
         {
-            return 1; 
-        }        
+            printf("Incorrect\n");
+        // mark as incorrect somehow       
+            printf("answer: %s", answr);
+            score--;
+        }
+    if (score == numquestions )
+        {
+            printf("Congratulations on finishing you scored: %d out of %d", score, numquestions);
+            printf("Would you like to play again? (y/n)\n");
+            char playagain[2];
+            scanf("%c", &playagain);
+            if ((playagain == "y"))
+            {
+                score = 0;
+            }
+            else
+            {
+                free(question);
+                free(answr);
+                free(usransw);
+                fclose(file);
+                return 1; 
+            }        
+        }
     }
-    }
-}
